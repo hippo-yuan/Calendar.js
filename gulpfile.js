beb13dc20807b1f85c86dd7c1ebe75b61412f29a
hippo-yuan <1943595495@qq.com>
2016-04-12 01:34:39 +0800

    v0.1.0

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require("gulp"),
+	gulpLoadPlugins = require('gulp-load-plugins'),
+    del = require("del"),
+    Browsersync = require('browser-sync').create(),
+    reload = Browsersync.reload;
+var concat = require("gulp-concat");
+const $ =  gulpLoadPlugins();
+gulp.task("less", function() {
+	return gulp.src("./lib/styles/less/*.less")
+			   .pipe($.less())
+			   .pipe($.autoprefixer())
+			   .pipe(gulp.dest("./lib/styles/css"))
+			   .pipe(reload({stream: true}));
+});
+gulp.task("js", function() {
+	return gulp.src("./lib/scripts/*.js")
+			   .pipe($.jshint(".jshintrc"))
+			   .pipe($.jshint.reporter('default'))
+			   .pipe($.uglify())
+			   .pipe(gulp.dest("./js"));
+});
+gulp.task("clean", function() {
+	//del(['./styles/**/*.css']);
+});
+gulp.task("build-css", function() {
+	return gulp.src(["./lib/styles/css/reset.css", "./lib/styles/css/base.css", "./lib/styles/css/index.css"])
+			   .pipe($.concat("canlendar.css"))
+			   .pipe($.autoprefixer())
+			   .pipe(gulp.dest("./build"));
+})
+gulp.task("build", ["build-css"], function() {
+	return gulp.src("./lib/scripts/*.js")
+			   .pipe($.jshint.reporter('default'))
+			   .pipe($.uglify())
+			   .pipe(gulp.dest("./build"));
+});
+gulp.task('server', ['less', 'js'], function() {
+    Browsersync.init({
+        server: {
+            baseDir: "./test"
+        }
+    });
+    gulp.watch("./lib/styles/less/*.less", ['less']);
+    gulp.watch("*.html").on("change", reload);
+});
